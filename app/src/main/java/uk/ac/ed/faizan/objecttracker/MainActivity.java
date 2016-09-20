aece6ce38d3bdb1ff0b28ff44bef6837b6277dcb
Faizan Tahir <ftahir192@gmail.com>
2016-09-21 00:09:35 +0100

    Added camera activity startup when "Start Tracking" is pressed.
    Added functionality such that when the "Start Recording" button is
    pressed, a new activity called TrackingActivity.java is started, and this
    opens a camera preview based on the color camera.
    
    To-do: Add options to the camera preview such as changing to fish-eye
    camera, changing ISO on color camera, adding record buttons, etc.
    Finalizing user interface on mockup.

--- app/src/main/java/uk/ac/ed/faizan/objecttracker/MainActivity.java
+++ app/src/main/java/uk/ac/ed/faizan/objecttracker/MainActivity.java
@@ -1,13 +1,51 @@
 package uk.ac.ed.faizan.objecttracker;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Find views in activity_main.xml identified by the ID specified in the parameters
+        Button startTrackingButton = (Button) findViewById(R.id.start_tracking_button);
+        Button viewRecordingsButton = (Button) findViewById(R.id.view_recordings_button);
+        Button preferencesButton = (Button) findViewById(R.id.preferences_button);
+
+        /* Attach an onClickListener to each of the 3 buttons to detect clicks and carry out
+        relevant actions
+         */
+        startTrackingButton.setOnClickListener(this);
+        viewRecordingsButton.setOnClickListener(this);
+        preferencesButton.setOnClickListener(this);
+
+
     }
+
+
+    @Override
+    public void onClick(View view) {
+
+        switch(view.getId()) {
+            case R.id.start_tracking_button:
+                Intent i = new Intent(this, TrackingActivity.class);
+                startActivity(i);
+                break;
+            case R.id.view_recordings_button:
+                // Start an intent to open the file storage system
+                break;
+            case R.id.preferences_button:
+                // Start an intent to open the preferences window (may be removed)
+                break;
+            default:
+                break;
+        }
+    }
+
 }
---
+++ app/src/main/java/uk/ac/ed/faizan/objecttracker/TrackingActivity.java
@@ -0,0 +1,129 @@
+package uk.ac.ed.faizan.objecttracker;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.atap.tangoservice.Tango;
+import com.google.atap.tangoservice.TangoCameraIntrinsics;
+import com.google.atap.tangoservice.TangoCameraPreview;
+import com.google.atap.tangoservice.TangoConfig;
+import com.google.atap.tangoservice.TangoCoordinateFramePair;
+import com.google.atap.tangoservice.TangoErrorException;
+import com.google.atap.tangoservice.TangoEvent;
+import com.google.atap.tangoservice.TangoOutOfDateException;
+import com.google.atap.tangoservice.TangoPointCloudData;
+import com.google.atap.tangoservice.TangoPoseData;
+import com.google.atap.tangoservice.TangoXyzIjData;
+
+import java.util.ArrayList;
+
+public class TrackingActivity extends Activity {
+
+
+    final static String TAG = "cameraPreview"; // For debugging purposes
+    TangoCameraPreview mTangoCameraPreview;
+    Tango mTango;
+    boolean mTangoIsConnected;
+
+    // Inflate the layout and set the camera view when activity is created
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tracking);
+
+        // Initialize the Tango's camera view to the TangoCameraPreview defined in activity_tracking.xml
+        mTangoCameraPreview = (TangoCameraPreview) findViewById(R.id.camera_preview);
+    }
+
+    // Called after onCreate() in an Android activity lifecycle.
+    @Override
+    protected void onResume() {
+        Log.i(TAG, "In resume");
+        super.onResume();
+
+        // Initialize Tango as a normal service.
+        // Because we call mTango.disconnect() in onPause, the Tango service will unbind each
+        // time the activity is obscured, so we must check to see if it needs binded again each time
+        // onResume is called.
+        if (!mTangoIsConnected) {
+            // ***********New thread possibly not needed here.*********
+            mTango = new Tango(TrackingActivity.this, new Runnable() {
+
+                @Override
+                public void run() {
+                    try {
+                        startTrackingPreview();
+                    } catch (TangoOutOfDateException e) {
+                        Log.e(TAG, "Tango out-of-date exception");
+                    } catch (TangoErrorException e) {
+                        Log.e(TAG, "Tango error exception");
+                    }
+                }
+            });
+
+        }
+    }
+
+    // Called when activity becomes obscured.
+    @Override
+    protected void onPause() {
+        // If the camera is connected, then disconnect the camera and unbind the Tango service
+        super.onPause();
+        if (mTangoIsConnected) {
+            mTango.disconnect();
+            mTangoCameraPreview.disconnectFromTangoCamera();
+            mTangoIsConnected = false;
+        }
+    }
+
+    // Called within the onResume() method, and is responsible for connecting to the camera
+    // and binding the Tango service.
+    public void startTrackingPreview() {
+
+        // Connect to the Tango Color (RGB) camera
+        mTangoCameraPreview.connectToTangoCamera(mTango, TangoCameraIntrinsics.TANGO_CAMERA_COLOR);
+        // Bind the Tango service with default settings
+        mTango.connect(mTango.getConfig(TangoConfig.CONFIG_TYPE_DEFAULT));
+        mTangoIsConnected = true;
+
+        // No need to add any coordinate frame pairs since we are not using
+        // pose data. So just initialize.
+        ArrayList<TangoCoordinateFramePair> framePairs = new ArrayList<TangoCoordinateFramePair>();
+        mTango.connectListener(framePairs, new Tango.OnTangoUpdateListener() {
+            @Override
+            public void onPoseAvailable(TangoPoseData pose) {
+                // We are not using OnPoseAvailable for this app
+            }
+
+            @Override
+            public void onFrameAvailable(int cameraId) {
+
+                // Check if the frame available is for the camera we want and
+                // update its frame on the camera preview.
+                if (cameraId == TangoCameraIntrinsics.TANGO_CAMERA_COLOR) {
+                    mTangoCameraPreview.onFrameAvailable();
+                    // Can also add OpenCV code here.
+                }
+            }
+
+            @Override
+            public void onXyzIjAvailable(TangoXyzIjData xyzIj) {
+                // We are not using onXyzIjAvailable for this app.
+            }
+
+            @Override
+            public void onPointCloudAvailable(TangoPointCloudData pointCloud) {
+                // We are not using OnPoseAvailable for this app
+            }
+
+            @Override
+            public void onTangoEvent(TangoEvent event) {
+                // We are not using OnPoseAvailable for this app
+            }
+        });
+    }
+
+
+
+}
