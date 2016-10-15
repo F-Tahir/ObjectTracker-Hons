faa8dd5b24f003c779e4fdb3ac1f9e6cb383ee79
Faizan Tahir <ftahir192@gmail.com>
2016-10-15 18:38:04 +0100

    Readded the rendering to mSurface code in CameraBridgeViewBase

--- openCVLibrary/src/main/java/org/opencv/android/CameraBridgeViewBase.java
+++ openCVLibrary/src/main/java/org/opencv/android/CameraBridgeViewBase.java
@@ -464,6 +464,38 @@ public abstract class CameraBridgeViewBase extends SurfaceView implements Surfac
             }
         }
 
+        if (bmpValid && mCacheBitmap != null) {
+            Canvas canvas;
+
+            if (mRecorder != null && mSurface != null) {
+
+                canvas = mSurface.lockCanvas(null);
+
+                canvas.drawColor(0, android.graphics.PorterDuff.Mode.CLEAR);
+                Log.d(TAG, "mStretch value: " + mScale);
+
+                if (mScale != 0) {
+                    canvas.drawBitmap(mCacheBitmap, new Rect(0,0,mCacheBitmap.getWidth(), mCacheBitmap.getHeight()),
+                            new Rect((int)((canvas.getWidth() - mScale*mCacheBitmap.getWidth()) / 2),
+                                    (int)((canvas.getHeight() - mScale*mCacheBitmap.getHeight()) / 2),
+                                    (int)((canvas.getWidth() - mScale*mCacheBitmap.getWidth()) / 2 + mScale*mCacheBitmap.getWidth()),
+                                    (int)((canvas.getHeight() - mScale*mCacheBitmap.getHeight()) / 2 + mScale*mCacheBitmap.getHeight())), null);
+                } else {
+                    canvas.drawBitmap(mCacheBitmap, new Rect(0,0,mCacheBitmap.getWidth(), mCacheBitmap.getHeight()),
+                            new Rect((canvas.getWidth() - mCacheBitmap.getWidth()) / 2,
+                                    (canvas.getHeight() - mCacheBitmap.getHeight()) / 2,
+                                    (canvas.getWidth() - mCacheBitmap.getWidth()) / 2 + mCacheBitmap.getWidth(),
+                                    (canvas.getHeight() - mCacheBitmap.getHeight()) / 2 + mCacheBitmap.getHeight()), null);
+                }
+
+                if (mFpsMeter != null) {
+                    mFpsMeter.measure();
+                    mFpsMeter.draw(canvas, 20, 30);
+                }
+                mSurface.unlockCanvasAndPost(canvas);
+            }
+
+        }
 
     }
 
