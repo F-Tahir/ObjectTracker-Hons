1a2001d781f05a0dd2e3b5ab62fd3c8634bea3dc
Faizan Tahir <ftahir192@gmail.com>
2017-02-16 21:53:22 +0000

    Wrote code to evaluate tracker performance.
    Wrote a script that allows me to loop through a recorded video, and manually select the object position, frame by frame, by clicking.
    The locations are then saved to a yml file. I also wrote a script that compares the outputs from this yml file against the
    original file obtained from the android app, and calculates Euclidean distance. Will also add other heuristics.

---
+++ Evaluation Scripts/Tracker Evaluation/EvaluateTracker.py
@@ -0,0 +1,72 @@
+# This script allows me to loop through a video frame-by-frame, and click on
+# the location of the object, with 100% accuracy. The locations are appended
+# to an output file, and can be used for evaluating the accuracy of my manual
+# or automatic tracking
+import optparse
+import math
+
+
+optparser = optparse.OptionParser()
+optparser.add_option("-m", "--man", dest="man", default="rec2output.yml",
+    help="Original YML file (with automatic tracking results from app)")
+optparser.add_option("-a", "--aut", dest="aut", default="../Data/rec2.yml",
+    help="New YML file (with manual tracking results from GetFrameData.py)")
+optparser.add_option("-f", "--frames", dest="frames", default=2000, type="int", help="Number of frames to analyze")
+optparser.add_option("-t", "--type", dest="type", default="euclid", help="Evaluation heuristic")
+opts = optparser.parse_args()[0]
+
+
+manfile = open(opts.man, "r")
+autofile = open(opts.aut, "r")
+
+
+# Stores tuples in the form (framecount, (x, y))
+man_results = list()
+auto_results = list()
+distance  = 0
+
+# Loop through the manually obtained results and store them in tuples of above form
+for line in manfile:
+    if "framestamp" in line:
+        framestamp = int(line.strip().split(":")[1])
+        x = int(manfile.next().strip().split(":")[1])
+        y = int(manfile.next().strip().split(":")[1])
+        man_results.append((framestamp, (x, y)))
+
+        if framestamp == opts.frames:
+            break
+
+# Loop through the automatically obtained results (from automatic tracking in
+# android app) and store then in tuples of the above form
+for line in autofile:
+    if "framestamp" in line:
+        framestamp = int(line.strip().split(":")[1])
+        # Skip timestamp and object_position header
+        autofile.next()
+        autofile.next()
+
+        x = int(autofile.next().strip().split(":")[1])
+        y = int(autofile.next().strip().split(":")[1])
+        auto_results.append((framestamp, (x, y)))
+
+        # Only need same amount of tuples as man_results
+        if framestamp == len(man_results) or framestamp == opts.frames:
+            break
+
+
+print man_results[1][1][0]
+
+# Calculate Euclidean distance between points in same frame correspondence, between
+# automatic and manual tracking. Sum up and take average.
+if opts.type == "euclid":
+    for i in xrange(0, len(man_results)):
+        lhs = (man_results[i][1][0] - auto_results[i][1][0])**2
+        rhs = (man_results[i][1][1] - auto_results[i][1][1])**2
+        distance += math.sqrt(lhs + rhs)
+
+    print "Evaluation using Euclidean distance is %s" % str(distance/len(man_results))
+    # Possibly print other valuable heuristic measurements too.
+
+else:
+    # Manning distance/city block distance/etc?
+    print "Undefined method"
---
+++ Evaluation Scripts/Tracker Evaluation/GetFrameData.py
@@ -0,0 +1,66 @@
+# This script allows me to loop through a video frame-by-frame, and click on
+# the location of the object, with 100% accuracy. The locations are appended
+# to an output file, and can be used for evaluating the accuracy of my manual
+# or automatic tracking
+import cv2
+import optparse
+
+
+optparser = optparse.OptionParser()
+optparser.add_option("-r", "--rec", dest="rec", default="rec1.mp4", help="Recording to analyse.")
+optparser.add_option("-o", "--out", dest="out", default="rec1output.yml", help="Output file to store data in")
+optparser.add_option("-f", "--frames", dest="frames", default=2000, type="int", help="Number of frames to analyze")
+opts = optparser.parse_args()[0]
+
+
+cap = cv2.VideoCapture(opts.rec)
+
+
+cv2.namedWindow('video')
+
+
+global frame, framecount, f
+f = open(opts.out, 'a')
+framecount = 0
+frame = None
+
+
+# Function called each time mouse is clicked down. Store x and y coordinates,
+# with framestamp in a yml file
+def get_coord(event, x, y, flags, param):
+    global frame, framecount
+    if event == cv2.EVENT_LBUTTONDOWN:
+
+        # First frame, initialise frame variable (currently it is None)
+        if framecount == 0:
+            ret, frame = cap.read()
+            framecount = 1
+        else:
+            # Update frame so that the loop below can show next image
+            ret, frame = cap.read()
+            f.write("framestamp:" + str(framecount) + "\n")
+            f.write("\tx:" + str(x) + "\n")
+            f.write("\ty:" + str(y) + "\n\n\n")
+            print framecount
+            framecount += 1
+
+
+# Set callback to call get_coord
+cv2.setMouseCallback('video', get_coord)
+
+
+# Infinite loop while cap is opened - frame is only updated on click
+while cap.isOpened():
+    if frame != None:
+        cv2.imshow('video', frame)
+
+    # Break if keyboard click
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        break
+
+    # Break after we have analyzed set number of frames
+    if framecount == opts.frames+1:
+        break
+
+cap.release()
+f.close()
