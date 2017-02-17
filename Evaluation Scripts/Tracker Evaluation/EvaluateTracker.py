2c763adccb651622930a6e756cd57c26daff9f8a
Faizan Tahir <ftahir192@gmail.com>
2017-02-17 02:23:11 +0000

    Added accuracy heuristic

--- Evaluation Scripts/Tracker Evaluation/EvaluateTracker.py
+++ Evaluation Scripts/Tracker Evaluation/EvaluateTracker.py
@@ -10,9 +10,11 @@ optparser = optparse.OptionParser()
 optparser.add_option("-m", "--man", dest="man", default="rec2output.yml",
     help="Original YML file (with automatic tracking results from app)")
 optparser.add_option("-a", "--aut", dest="aut", default="../Data/rec2.yml",
-    help="New YML file (with manual tracking results from GetFrameData.py)")
+    help="New YML file (with ground truth results from GetFrameData.py)")
+optparser.add_option("-k", "--heur", dest="heuristic", default="euclid",
+    help="Evaluation heuristic.")
 optparser.add_option("-f", "--frames", dest="frames", default=2000, type="int", help="Number of frames to analyze")
-optparser.add_option("-t", "--type", dest="type", default="euclid", help="Evaluation heuristic")
+optparser.add_option("-t", "--thresh", dest="threshold", default=20, type="int", help="Threshold for error detection.")
 opts = optparser.parse_args()[0]
 
 
@@ -23,9 +25,8 @@ autofile = open(opts.aut, "r")
 # Stores tuples in the form (framecount, (x, y))
 man_results = list()
 auto_results = list()
-distance  = 0
 
-# Loop through the manually obtained results and store them in tuples of above form
+# Loop through the manually obtained  (ground truth)results and store them in tuples of above form
 for line in manfile:
     if "framestamp" in line:
         framestamp = int(line.strip().split(":")[1])
@@ -54,17 +55,46 @@ for line in autofile:
             break
 
 
-print man_results[1][1][0]
+
+
+# Print heuristics
+print "\n\n######################################################################"
+print "                     EVALUATION SUMMMARY"
+print "######################################################################\n"
+print "Evaluation method: %s" % opts.heuristic
+
 
 # Calculate Euclidean distance between points in same frame correspondence, between
 # automatic and manual tracking. Sum up and take average.
-if opts.type == "euclid":
+if opts.heuristic == "euclid":
+    distance = 0
     distance += sum(math.hypot(man[1][0] - auto[1][0],
         man[1][1] - auto[1][1]) for man, auto in zip (man_results, auto_results))
+    print """Description: This heuristic method computes the euclidean distance between
+    the predicted position and the ground truth position, for each frame, and then
+    takes an average.\n"""
+    print "Total # Frames: %s" % str(len(man_results))
+    print "Average Euclidean Distance: %s\n\n" % str(distance/len(man_results))
 
-    print "Evaluation using Euclidean distance is %s" % str(distance/len(man_results))
-    # Possibly print other valuable heuristic measurements too.
+# Return number of frames where tracking is accurate/inaccurate by measuring
+# euclidean distance from predicted position to ground truth position.
+elif opts.heuristic == "accuracy":
+    accurate = 0
+    inaccurate = 0
+    for i in xrange(0, len(man_results)):
+        lhs = (man_results[i][1][0] - auto_results[i][1][0])**2
+        rhs = (man_results[i][1][1] - auto_results[i][1][1])**2
+        distance = math.sqrt(lhs + rhs)
 
-else:
-    # Manning distance/city block distance/etc?
-    print "Undefined method"
+        if distance <= opts.threshold:
+            accurate += 1
+        else:
+            inaccurate += 1
+
+    print """Description: This heuristic method calculates the euclidean distance
+    between predicted and ground truth positions, for each frame. If the distance
+    is below a threshold, the frame is classed as accurate, otherwise it is inaccurate."""
+    print "Correct # frames: %s" % str(accurate)
+    print "Incorrect # frames: %s" % str(inaccurate)
+    print "Total # Frames: %s" % str(len(man_results))
+    print "Accuracy: %s\n\n" % str((float(accurate)/len(man_results))*100.0)
