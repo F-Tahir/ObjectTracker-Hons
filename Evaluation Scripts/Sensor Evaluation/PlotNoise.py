9225b090bdf76b49919ae8e8ed1fe14e2af66029
Faizan Tahir <ftahir192@gmail.com>
2017-02-25 23:21:45 +0000

    Finished coding PlotNoise.py

--- Evaluation Scripts/Sensor Evaluation/PlotNoise.py
+++ Evaluation Scripts/Sensor Evaluation/PlotNoise.py
@@ -6,32 +6,51 @@ import matplotlib.pyplot as plt
 import optparse
 
 optparser = optparse.OptionParser()
-optparser.add_option("-y", "--yml", dest="yml", default="../Data/accelerometer/phone_still1.yml", help="YML file needing to be parsed")
+optparser.add_option("-y", "--yml", dest="yml", default="../Data/accelerometer/accel_5000.yml", help="YML file needing to be parsed")
+optparser.add_option("-a", "--axis", dest="axis", default="../Data/accelerometer/accel_5000.yml", help="Axis to plot: x, y, z, all")
 opts = optparser.parse_args()[0]
 
 f = open(opts.yml, "r")
 
-t_acc_x = list()
-t_acc_y = list()
-t_acc_z = list()
-t_framestamp = list()
+t_x = list()
+t_y = list()
+t_z = list()
+t_iteration = list()
+
+# Skip over "General Debug Information" block
+f.next()
+f.next()
+f.next()
+f.next()
+
 
 for line in f:
-    # Step 1: Loop through all framestamp blocks and get the framestamp
-    if "framestamp" in line:
-        t_framestamp.append(int(line.strip().split(":")[1]))
-
-    # Step 2: Get the accelerometer values for each framestamp, across each axis.
-    if "accelerometer_values" in line:
-        t_acc_x.append(float(f.next().strip().split(":")[1]))
-        t_acc_y.append(float(f.next().strip().split(":")[1]))
-        t_acc_z.append(float(f.next().strip().split(":")[1]))
+    # Loop through all yml blocks and get iterations, and for that iteration,
+    # the sensor readings
+    if "iteration" in line:
+        t_iteration.append(int(line.strip().split(":")[1]))
+        t_x.append(float(f.next().strip().split(":")[1]))
+        t_y.append(float(f.next().strip().split(":")[1]))
+        t_z.append(float(f.next().strip().split(":")[1]))
 
 
-plt.plot(t_framestamp, t_acc_x, label="x-axis readings")
-plt.plot(t_framestamp, t_acc_y, label="y-axis readings")
-plt.plot(t_framestamp, t_acc_z, label="z-axis readings")
-plt.title("Accelerometer noise")
+# Plot sensor data for each iteration.
+
+if (opts.axis is "x"):
+    plt.plot(t_iteration, t_x, label="x-axis readings")
+
+if (opts.axis is "y"):
+    plt.plot(t_iteration, t_y, label="y-axis readings")
+
+if (opts.axis is "z"):
+    plt.plot(t_iteration, t_z, label="z-axis readings")
+
+if (opts.axis is "all"):
+    plt.plot(t_iteration, t_x, label="x-axis readings")
+    plt.plot(t_iteration, t_y, label="y-axis readings")
+    plt.plot(t_iteration, t_z, label="z-axis readings")
+
+plt.title("Sensor Data")
 plt.xlabel("Frame Number")
 plt.ylabel("Sensor Value")
 plt.legend()
