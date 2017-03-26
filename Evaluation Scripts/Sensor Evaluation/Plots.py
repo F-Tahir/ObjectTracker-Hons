aaa217bf56cccab613ac8af9512af16904cb4b9a
Faizan Tahir <ftahir192@gmail.com>
2017-03-26 18:16:24 +0100

    Updated plots.py

--- Evaluation Scripts/Sensor Evaluation/Plots.py
+++ Evaluation Scripts/Sensor Evaluation/Plots.py
@@ -33,36 +33,28 @@ for line in f:
         f.next()
         f.next()
         f.next()
-        # Get the tx and ty readings, store in a tuple and append to list
+        # Get the tx and ty readings, store in a tuple and append to listwoAccelerometer Readings")
         phone_tx = float(f.next().strip().split(":")[1])
         phone_ty = float(f.next().strip().split(":")[1])
         t_vicon.append((phone_tx, phone_ty))
 
-print opts.yml
-print t_acc
-
-# Plot the points in t_acc
-x_acc = [i[0] for i in t_vicon]
-y_acc = [i[1] for i in t_vicon]
-acc_graph = plt.figure(1)
-acc_graph.canvas.set_window_title("Accelerometer Readings")
-plt.plot(x_acc, y_acc)
-plt.axis([-1500, 2500, -1500, 2500])
-
-# # PLot the points in t_vicon
-# x_vicon = [i[0] for i in t_vicon]
-# y_vicon = [i[1] for i in t_vicon]
-# vicon_graph = plt.figure(2)
-# vicon_graph.canvas.set_window_title("Vicon Readings")
-# plt.plot(x_vicon, y_vicon)
+# plt.plot(x_acc, y_acc)
 # plt.axis([-1500, 2500, -1500, 2500])
+
+# PLot the points in t_vicon
+x_vicon = [i[0] for i in t_vicon]
+y_vicon = [i[1] for i in t_vicon]
+vicon_graph = plt.figure(2)
+vicon_graph.canvas.set_window_title("Vicon Readings")
+plt.plot(x_vicon, y_vicon)
+plt.axis([-1500, 2500, -1500, 2500])
 
 plt.show()
 
 # Step 3: Compute optimal affine transformation between T_acc and T_vicon
 # => Returns a 2x3 matrix containing rotaional matrix and translation matrix
-m = cv2.estimateRigidTransform(np.asarray(t_acc), np.asarray(t_vicon), True)
-print m # Returns None
+# m = cv2.estimateRigidTransform(np.asarray(t_acc), np.asarray(t_vicon), True)
+# print m # Returns None
 
 # Step 4: Transform t_acc using the transformation matrix m
 # t_acc_transformed = cv2.transform(np.asarray(t_acc), m)
