d1dbc6c79ac9d7a15f908f286ff4beb281dc887e
Faizan Tahir <ftahir192@gmail.com>
2017-03-27 01:27:03 +0100

    Integration of sensor noise

--- Evaluation Scripts/Sensor Evaluation/PlotNoise.py
+++ Evaluation Scripts/Sensor Evaluation/PlotNoise.py
@@ -4,10 +4,11 @@
 import numpy as np
 import optparse
 import matplotlib.pyplot as plt
+from scipy.integrate import cumtrapz
 
 
 optparser = optparse.OptionParser()
-optparser.add_option("-y", "--yml", dest="yml", default="../Data/noise/20170324_192900_DUMP.yml", help="YML file needing to be parsed")
+optparser.add_option("-y", "--yml", dest="yml", default="../Data/noise/20170324_192850_DUMP.yml", help="YML file needing to be parsed")
 optparser.add_option("-a", "--axis", dest="axis", default="x", help="Axis to plot: x, y, z, all")
 opts = optparser.parse_args()[0]
 
@@ -17,12 +18,16 @@ t_x = list()
 t_y = list()
 t_z = list()
 t_iteration = list()
+t_velocity = list()
+t_position = list()
+t_iteration_s = list()
 
 # Skip over "General Debug Information" block
 f.next()
 f.next()
 f.next()
 f.next()
+
 
 
 for line in f:
@@ -34,9 +39,18 @@ for line in f:
         t_y.append(float(f.next().strip().split(":")[1]))
         t_z.append(float(f.next().strip().split(":")[1]))
 
+for i in range(0, len(t_iteration)):
+    t_iteration_s.append(t_iteration[i]/1000.0)
+
+print len(t_iteration_s)
+
+
+# Double integrating to obtain positions
+t_velocity = cumtrapz(t_y, t_iteration_s, initial=0)
+t_position = cumtrapz(t_velocity, t_iteration_s, initial=0)
+
 
 # Plot sensor data for each iteration.
-
 if (opts.axis == "x"):
     plt.plot(t_iteration, t_x, label="x-axis readings")
 
@@ -57,6 +71,6 @@ if (opts.axis == "all"):
     plt.legend()
 
 plt.xlabel("Time (milliseconds)")
-plt.ylabel("Accelerometer Values (y-axis)")
+plt.ylabel("Gyroscope Noise (y-axis)")
 
 plt.show()
